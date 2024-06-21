1808db49d73e2f04ed62e19ea77d5a4fe2cf7f1f
pierre.maunier <pierre.maunier@epita.fr>
2024-06-21 15:59:07 +0200

    fix(bruteforce)

--- src/bruteForce.py
+++ src/bruteForce.py
@@ -3,9 +3,9 @@ def bruteForceRec(unit_list, trait_list, index):
 	if (index == len(unit_list)):
 		return 0
 	if (unit_list[index].compatible(trait_list)):
-		E = trait_list + unit_list.traits
-		return max(1 + bruteForce(unit_list, E, index + 1), bruteForce(unit_list, trait_list, index + 1))
-	return bruteForce(unit_list, trait_list, index + 1)
+		E = trait_list + unit_list[index].traits
+		return max(1 + bruteForceRec(unit_list, E, index + 1), bruteForceRec(unit_list, trait_list, index + 1))
+	return bruteForceRec(unit_list, trait_list, index + 1)
 
 
 def bruteForce(unit_list):
