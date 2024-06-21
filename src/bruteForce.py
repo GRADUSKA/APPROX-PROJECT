c9ed46d089885cf92d015b0037fa6f6365d033b3
pierre.maunier <pierre.maunier@epita.fr>
2024-06-21 15:43:20 +0200

    fix(bruteForce): added hat function for bruteforce

--- src/bruteForce.py
+++ src/bruteForce.py
@@ -1,8 +1,13 @@
-def bruteForce(unit_list, trait_list, index):
+def bruteForceRec(unit_list, trait_list, index):
 	
-	if (index == len(unit_list))
+	if (index == len(unit_list)):
 		return 0
 	if (unit_list[index].compatible(trait_list)):
 		E = trait_list + unit_list.traits
 		return max(1 + bruteForce(unit_list, E, index + 1), bruteForce(unit_list, trait_list, index + 1))
 	return bruteForce(unit_list, trait_list, index + 1)
+
+
+def bruteForce(unit_list, trait_list):
+
+	return bruteForceRec(unit_list, trait_list, 0)
