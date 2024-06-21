3799aac16f553725f3bc7bbd0eeaac7fc0a77f92
Louis Le Quellec <louis.le-quellec@epita.fr>
2024-06-21 16:03:34 +0200

    fix: neighbors

--- src/first_implem.py
+++ src/first_implem.py
@@ -18,12 +18,12 @@ def compute(G):
     while G.edges():
         node = None
         for n in G.nodes():
-            if node == None or len(G.neighbors(n)) < len(G.neighbors(node)):
+            if node == None or len([a for a in G.neighbors(n)]) < len([b for b in G.neighbors(node)]):
                 node = n
         if len(G.neighbors(node)) != 0:
             other = None
             for n in G.neighbors(node):
-                if other == None or len(G.neighbors(n)) < len(G.neighbors(other)):
+                if other == None or len([c for c in G.neighbors(n)]) < len([d for d in G.neighbors(other)]):
                     other = n
             result.append(G.get_edge_data(champion.traits[0], champion.traits[1])['champ'])
             G.remove_node(node)
