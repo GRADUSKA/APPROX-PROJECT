a3e907c8e9e89b712a23854fa8e96002c564f809
Louis Le Quellec <louis.le-quellec@epita.fr>
2024-06-21 16:06:47 +0200

    fix: node names

--- src/first_implem.py
+++ src/first_implem.py
@@ -25,7 +25,7 @@ def compute(G):
             for n in G.neighbors(node):
                 if other == None or len([c for c in G.neighbors(n)]) < len([d for d in G.neighbors(other)]):
                     other = n
-            result.append(G.get_edge_data(champion.traits[0], champion.traits[1])['champ'])
+            result.append(G.get_edge_data(node, other)['champ'])
             G.remove_node(node)
             G.remove_node(other)
         else:
