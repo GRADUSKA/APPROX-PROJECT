3dd12264914a670b7f496ff73ebe265dd0a4e02f
Antoine Thomas <antoine.thomas@epita.fr>
2024-06-22 16:10:02 +0200

    feat: optimisation of bruteForce (not much)

--- src/bruteForce.py
+++ src/bruteForce.py
@@ -1,13 +1,11 @@
-def bruteForceRec(unit_list, trait_list, index):
-	
+def bruteForceRec(unit_list, trait_set, index):
 	if (index == len(unit_list)):
 		return 0
-	if (unit_list[index].compatible(trait_list)):
-		E = trait_list + unit_list[index].traits
-		return max(1 + bruteForceRec(unit_list, E, index + 1), bruteForceRec(unit_list, trait_list, index + 1))
-	return bruteForceRec(unit_list, trait_list, index + 1)
+	if (unit_list[index].compatible(trait_set)):
+		E = trait_set | set(unit_list[index].traits)
+		return max(1 + bruteForceRec(unit_list, E, index + 1), bruteForceRec(unit_list, trait_set, index + 1))
+	return bruteForceRec(unit_list, trait_set, index + 1)
 
 
 def bruteForce(unit_list):
-
-	return bruteForceRec(unit_list, [], 0)
+	return bruteForceRec(unit_list, set(), 0)
