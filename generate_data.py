2dce4fc5ab199d646d1759bb09fa4e6ab0b868f3
Antoine Thomas <antoine.thomas@epita.fr>
2024-06-22 14:59:49 +0200

    feat: change README

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ import random
 origins = ["Fated", "Ghostly", "Mythic", "Umbral", "Storyweaver", "Inkshadow", "Heavenly", "Fortune", "Dryad", "Porcelain", "Dragonlord", "Lovers"]
 classes = ["Arcanist", "Sniper", "Behemoth", "Duelist", "Warden", "Reaper", "Bruiser", "Invoker", "Trickshot", "Sage", "Altruist", "Artist", "SpiritWalker"]
 
-# Listes de noms possibles (pour simplification, nous pouvons générer des noms comme Champ1, Champ2, etc.)
+# Listes de noms possibles (pour simplification, nous avons des noms comme Champ1, Champ2, etc.)
 num_champions = int(input())
 champions = []
 traits_per_champion = []
@@ -12,12 +12,12 @@ traits_per_champion = []
 for i in range(num_champions):
     champ_name = f"Champ{i+1}"
     
-    # Générer entre 1 et 3 traits pour chaque champion
+    # Générer entre 2 et 3 traits pour chaque champion
     num_traits = random.randint(2, 3)
     
     traits = set()
     while len(traits) < num_traits:
-        if len(traits) < 2:  # Ensure a mix of origins and classes
+        if len(traits) < 2:  # Pour avoir des classes et des origines
             trait_type = random.choice(["origin", "class"])
         else:
             trait_type = "any"
@@ -30,7 +30,7 @@ for i in range(num_champions):
     champions.append(champ_name)
     traits_per_champion.append(list(traits))
 
-# Formater les résultats selon la grammaire spécifiée
+# Formater les résultats selon la grammaire spécifiée soit nom_champion trait1 traits2 ... 
 formatted_data = "\n".join([f"{champ} {' '.join(traits)}" for champ, traits in zip(champions, traits_per_champion)])
 
 print(formatted_data)
